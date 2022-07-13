ac025d9bbc88c1e097099d6808859d45c22ef1e8
qope <toqe159@gmail.com>
2022-07-13 17:11:41 +0900

    impl

--- main.go
+++ main.go
@@ -1,18 +1,50 @@
 package main
 
 import (
-	"log"
+	"flag"
+	"fmt"
+	"os"
+	"os/signal"
+	"syscall"
 
-	badger "github.com/dgraph-io/badger/v3"
+	"github.com/dgraph-io/badger"
+
+	abciserver "github.com/tendermint/tendermint/abci/server"
+	"github.com/tendermint/tendermint/libs/log"
 )
 
+var socketAddr string
+
+func init() {
+	flag.StringVar(&socketAddr, "socket-addr", "unix://example.sock", "Unix domain socket address")
+}
+
 func main() {
-	// Open the Badger database located in the /tmp/badger directory.
-	// It will be created if it doesn't exist.
-	db, err := badger.Open(badger.DefaultOptions(".badger"))
+	db, err := badger.Open(badger.DefaultOptions("/tmp/badger"))
 	if err != nil {
-		log.Fatal(err)
+		fmt.Fprintf(os.Stderr, "failed to open badger db: %v", err)
+		os.Exit(1)
 	}
 	defer db.Close()
-	// Your code here…
+	app := NewKVStoreApplication(db)
+
+	flag.Parse()
+
+	logger, err := log.NewDefaultLogger(log.LogFormatPlain, log.LogLevelInfo, false)
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "failed to configure logger: %v", err)
+		os.Exit(1)
+	}
+
+	server := abciserver.NewSocketServer(socketAddr, app)
+	server.SetLogger(logger)
+	if err := server.Start(); err != nil {
+		fmt.Fprintf(os.Stderr, "error starting socket server: %v", err)
+		os.Exit(1)
+	}
+	defer server.Stop()
+
+	c := make(chan os.Signal, 1)
+	signal.Notify(c, os.Interrupt, syscall.SIGTERM)
+	<-c
 }
